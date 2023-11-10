fdc5dca1ba51201190a37076a538a2152fcb629e
SVI <shylika@i.ua>
2023-11-10 20:27:21 +0200

    FIX: redirect to my account

--- web/app/themes/graming/header.php
+++ web/app/themes/graming/header.php
@@ -34,7 +34,7 @@
 	<header class="header-section">
 		<div class="header">
 			<div class="header_logo">
-				<a class="site-logo" href="/">
+				<a class="site-logo" href="<?php if (!is_user_logged_in()): ?>/<?php else: ?>/my-account/<?php endif; ?>">
 					Graming <img src="<?php echo get_template_directory_uri() . '/src/images/logo.svg' ?>">
 				</a>
 			</div>
